b433eb60d97b023f3beec85068e144a5992f7e38
fairoozp <fairoozp97@gmail.com>
2021-05-18 13:24:16 +0530

    DataBase

--- DataBase/app/src/main/java/com/example/database/MainActivity2.kt
+++ DataBase/app/src/main/java/com/example/database/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
@@ -14,12 +15,19 @@ class MainActivity2 : AppCompatActivity() {
         val save_bt : Button = findViewById(R.id.save_bt)
         val clear_bt : Button = findViewById(R.id.clear_bt)
         val home_bt : Button = findViewById(R.id.home_bt)
+        val name : EditText = findViewById(R.id.name)
+        val phone :EditText = findViewById(R.id.phone)
+        val email : EditText = findViewById(R.id.email)
+        val address : EditText = findViewById(R.id.address)
 
         save_bt.setOnClickListener {
-
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
         }
         clear_bt.setOnClickListener {
-
+            name.text.clear()
+            phone.text.clear()
+            email.text.clear()
+            address.text.clear()
         }
         home_bt.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
