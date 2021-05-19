eea389d6927cd7b451d0cf8bee0f60b6fdb7afdf
fairoozp <fairoozp97@gmail.com>
2021-05-19 12:35:57 +0530

    Db_Helper

--- DataBase/app/src/main/java/com/example/database/MainActivity.kt
+++ DataBase/app/src/main/java/com/example/database/MainActivity.kt
@@ -3,20 +3,44 @@ package com.example.database
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    val read : Button = findViewById(R.id.read)
+    val add : FloatingActionButton = findViewById(R.id.add)
+    val view : TextView = findViewById(R.id.view)
+    val databaseHelper = DatabaseHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val add : FloatingActionButton = findViewById(R.id.add)
         add.setOnClickListener {
             startActivity( Intent(this, MainActivity2::class.java))
         }
-
+        read.setOnClickListener {
+            val data = databaseHelper.readData()
+            val stringBuffer = StringBuffer()
+            if (data != null && data.count>0){
+                while (data.moveToNext()){
+                    stringBuffer.append("ID : ${data.getString(0)}\n")
+                    stringBuffer.append("NAME : ${data.getString(1)}\n")
+                    stringBuffer.append("PHONE NUMBER : ${data.getString(2)}\n")
+                    stringBuffer.append("EMAIL : ${data.getString(3)}\n")
+                    stringBuffer.append("ADDRESS : ${data.getString(4)}\n")
+                }
+                view.text = stringBuffer.toString()
+                Toast.makeText(this, "Data Retrieved", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-
     override fun onBackPressed() {
         finishAffinity()
     }
--- DataBase/app/src/main/java/com/example/database/MainActivity2.kt
+++ DataBase/app/src/main/java/com/example/database/MainActivity2.kt
@@ -8,31 +8,49 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
+
+    val name : EditText = findViewById(R.id.name)
+    val phone :EditText = findViewById(R.id.phone)
+    val email : EditText = findViewById(R.id.email)
+    val address : EditText = findViewById(R.id.address)
+    val save_bt : Button = findViewById(R.id.save_bt)
+    val clear_bt : Button = findViewById(R.id.clear_bt)
+    val home_bt : Button = findViewById(R.id.home_bt)
+    val databaseHelper = DatabaseHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val save_bt : Button = findViewById(R.id.save_bt)
-        val clear_bt : Button = findViewById(R.id.clear_bt)
-        val home_bt : Button = findViewById(R.id.home_bt)
-        val name : EditText = findViewById(R.id.name)
-        val phone :EditText = findViewById(R.id.phone)
-        val email : EditText = findViewById(R.id.email)
-        val address : EditText = findViewById(R.id.address)
-
         save_bt.setOnClickListener {
-            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+            insert_data()
+            clear_all()
         }
         clear_bt.setOnClickListener {
-            name.text.clear()
-            phone.text.clear()
-            email.text.clear()
-            address.text.clear()
+            clear_all()
         }
         home_bt.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
         }
+    }
 
+    private fun insert_data() {
+        val name = name.text.toString()
+        val phone = phone.text.toString()
+        val email = email.text.toString()
+        val address = address.text.toString()
+        val result : Boolean = databaseHelper.insertData(name,phone,email,address)
+        when{
+            result -> Toast.makeText(applicationContext, "Data Saved", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun clear_all() {
+        name.text.clear()
+        phone.text.clear()
+        email.text.clear()
+        address.text.clear()
     }
 
     override fun onBackPressed() {
