ed290f9fecf50c1a974b6c2c39e701f1e1aea89b
fairoozp <fairoozp97@gmail.com>
2021-05-19 16:42:24 +0530

    firease

--- DataBase/app/src/main/java/com/example/database/MainActivity.kt
+++ DataBase/app/src/main/java/com/example/database/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
