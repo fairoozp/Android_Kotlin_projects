0956c4d8478869e6ae23161ec20cea8874a36053
Fairooz <fairoozp97@gmail.com>
2021-05-24 13:21:40 +0530

    firebase

--- Sticky_Notes/app/src/main/java/com/example/stickynotes/MainActivity.kt
+++ Sticky_Notes/app/src/main/java/com/example/stickynotes/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.stickynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
