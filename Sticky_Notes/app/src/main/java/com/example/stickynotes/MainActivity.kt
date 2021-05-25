c305bc6f397caf6f3b81722593dda9fee1167323
Fairooz <fairoozp97@gmail.com>
2021-05-25 11:01:33 +0530

    new activity

--- Sticky_Notes/app/src/main/java/com/example/stickynotes/MainActivity.kt
+++ Sticky_Notes/app/src/main/java/com/example/stickynotes/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.stickynotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addBt : FloatingActionButton = findViewById(R.id.addBt)
+        addBt.setOnClickListener { startActivity(Intent(this, AddNewNotes::class.java)) }
     }
 }
