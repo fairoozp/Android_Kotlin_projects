b0109e74ff73ff6794ee755895f54926e93181ee
Fairooz <fairoozp97@gmail.com>
2021-05-25 10:38:28 +0530

    new activity

---
+++ Sticky_Notes/app/src/main/java/com/example/stickynotes/AddNewNotes.kt
@@ -0,0 +1,11 @@
+package com.example.stickynotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewNotes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_notes)
+    }
+}
