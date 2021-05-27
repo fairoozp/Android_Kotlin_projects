54788975e56d42f14387eae7512e5047124c2adc
Fairooz <fairoozp97@gmail.com>
2021-05-27 15:48:16 +0530

    sqlite

--- Sticky_Notes/app/src/main/java/com/example/stickynotes/DatabaseHelper.kt
+++ Sticky_Notes/app/src/main/java/com/example/stickynotes/DatabaseHelper.kt
@@ -1,6 +1,8 @@
 package com.example.stickynotes
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.stickynotes.DatabaseContainer.StickyNotes.Companion.ID
@@ -19,6 +21,44 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
     }
+    //Insert
+    @Suppress("EqualsBetweenInconvertibleTypes")
+    fun insert(notes : String) : Boolean{
+        val db : SQLiteDatabase? = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(NOTES,notes)
+        val insertData = db!!.insert(TABLE_NAME,null,contentValues)
+        db.close()
+        return !insertData.equals(-1)
+    }
+
+    //Raed
+    fun read(): Cursor {
+        val db: SQLiteDatabase? = this.writableDatabase
+        return db!!.rawQuery("SELECT * FROM $TABLE_NAME", null)
+    }
+    //Delete
+    fun delete(id : String) : Boolean {
+        val db : SQLiteDatabase? = this.writableDatabase
+        val deleteData = db!!.delete(TABLE_NAME, "$ID=?", arrayOf(id))
+        return deleteData != -1
+    }
+    //Update
+    fun update (id : String , notes : String) : Boolean {
+        val db : SQLiteDatabase? = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(NOTES,notes)
+        val updateData = db!!.update(TABLE_NAME, contentValues , "$ID=?",arrayOf(id))
+        db.close()
+        return updateData != -1
+    }
+    //DeleteAll
+    fun deleteAll(){
+        val  db : SQLiteDatabase? = this.writableDatabase
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
     companion object{
         private const val DATABASE_NAME = "Person.db"
         private const val DATABASE_VERSION = 1
