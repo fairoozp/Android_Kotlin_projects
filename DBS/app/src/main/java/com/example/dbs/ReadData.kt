8b1455e38355ec6f14597f21f70d201a04b25881
Fairooz <fairoozp97@gmail.com>
2021-06-17 21:02:02 +0530

    Update ReadData.kt

--- DBS/app/src/main/java/com/example/dbs/ReadData.kt
+++ DBS/app/src/main/java/com/example/dbs/ReadData.kt
@@ -1,22 +1,23 @@
 package com.example.dbs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ReadData : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_data)
-        
-        val download :ImageButton = findViewById(R.id.download)
-        download.setOnClickListener { Toast.makeText(this, "Wait For This Feature", Toast.LENGTH_SHORT).show() }
 
         val databaseHelper = DatabaseHelper(this)
         val data = databaseHelper.readData()
         val view : TextView = findViewById(R.id.view)
+        val download :ImageButton = findViewById(R.id.download)
         val stringBuffer = StringBuffer()
         if (data.count>0){
             while (data.moveToNext()){
@@ -35,6 +36,15 @@ class ReadData : AppCompatActivity() {
         }
         else{
             Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show()
+        }
+
+        download.setOnClickListener {
+            val textData = view.text
+            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("text", textData)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "Wait For This Feature", Toast.LENGTH_SHORT).show()
         }
 
     }
