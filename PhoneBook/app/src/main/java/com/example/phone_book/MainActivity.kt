ddeef4f332d58c38110f9c4823a5c8b1d9f49494
Fairooz <fairoozp97@gmail.com>
2021-09-16 13:51:05 +0530

    fragment

--- PhoneBook/app/src/main/java/com/example/phone_book/MainActivity.kt
+++ PhoneBook/app/src/main/java/com/example/phone_book/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.phone_book
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentView : FrameLayout = findViewById(R.id.fragment_view)
+
+
     }
 }
