3fd04ec403ec43405933989d2d56b4dc2bdec525
Fairooz <fairoozp97@gmail.com>
2021-10-05 13:09:38 +0530

    layout

--- PhoneBook/app/src/main/java/com/example/phone_book/MainActivity.kt
+++ PhoneBook/app/src/main/java/com/example/phone_book/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.phone_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
-import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentView : FrameLayout = findViewById(R.id.fragment_view)
-
-
+        val floatingActionButton : FloatingActionButton = findViewById(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener {
+            startActivity(Intent(this, Add::class.java))
+        }
 
     }
 }
